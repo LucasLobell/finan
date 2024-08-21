2041be252974413a1e3e8a2aafdf0e15fa1c9bd0
Lucas Lobell <lucaslobell2@gmail.com>
2024-08-21 17:34:03 -0300

    home, database, authentication, start dashboard

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+export default {
+    dialect: "postgresql",
+    schema: "./utils/schema.jsx",
+    out: "./drizzle",
+    dbCredentials: {
+        url: 'postgresql://neondb_owner:DmeFbis1S4rh@ep-royal-cake-a59ngg1u.us-east-2.aws.neon.tech/neondb?sslmode=require',
+        connectionStrings: 'postgresql://neondb_owner:DmeFbis1S4rh@ep-royal-cake-a59ngg1u.us-east-2.aws.neon.tech/neondb?sslmode=require',
+    },
+};
